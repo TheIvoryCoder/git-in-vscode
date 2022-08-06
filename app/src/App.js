a7ecd89144604ad034d7023caaac3a32bb77dc02
Charlie Cureton <ccureton21@gmail.com>
2022-08-06 08:44:40 +0100

    Initial Commit

---
+++ app/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Using Git in VSCode.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/theivorycoder/git-in-vscode/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://github.com/theivorycoder/git-in-vscode/
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
